9f32f6c9d6e70072b0e60c0447673d0381974b19
William Sebastian <okeefesebastian91@gmail.com>
2025-05-12 16:58:42 +0900

    complete longest word game challenge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,9 +4,13 @@ require "open-uri"
 class GamesController < ApplicationController
   def new
     @letters = ('a'..'z').to_a.sample(10)
+    @starting = Time.now
   end
 
   def score
+    session[:score] = 0 unless session[:score]
+    @ending = Time.now
+    @current_score = 0
     @word = params["word"]
     url = "https://dictionary.lewagon.com/#{@word}"
     user_serialized = URI.parse(url).read
@@ -17,10 +21,16 @@ class GamesController < ApplicationController
     end
     if user["found"] && is_valid
       @result = "Congratulations! #{@word} is a valid word!"
+      if @ending - params[:starting].to_datetime < 5
+        @current_score += (5 + @word.length)
+      else
+        @current_score += @word.length
+      end
     elsif user["found"] && !is_valid
       @result = "Sorry, but #{@word} can't be built out of #{params[:letters]}."
     else
       @result = "Sorry, but #{@word} doesn't seem to be a valid English word"
     end
+    session[:score] += @current_score
   end
 end
